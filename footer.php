6155d965ba2aacb5226be161f2ab699eb3a3f8aa
Shivang Chauhan <38112760+shivangchauhan0@users.noreply.github.com>
2021-03-05 20:53:23 +0530

    Boilerplate added

---
+++ footer.php
@@ -0,0 +1,10 @@
+</body>
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- Script JS -->
+<script src="JS/script.js"></script>
+<!-- Jquery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+</html>
---
+++ header.php
@@ -0,0 +1,22 @@
+<?php 
+  include("server.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- Semantic -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" integrity="sha512-8bHTC73gkZ7rZ7vpqUQThUDhqcNFyYi2xgDgPDHc+GXVGHXq+xPjynxIopALmOPqzo9JZj0k6OqqewdGO3EsrQ==" crossorigin="anonymous" />
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="vendor/fontawesome/css/all.css">
+    <!-- Font <Nunito and Open-sans>-->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;800;900&family=Open+Sans:wght@400;600&family=Reenie+Beanie&display=swap" rel="stylesheet">
+    <!-- style css -->
+    <link rel="stylesheet" href="CSS/style.css">
+    <title>Up Process</title>
+</head>
+<body>
