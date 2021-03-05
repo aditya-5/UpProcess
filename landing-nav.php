51cc433298efe2a5633b53881dd02f114dc76446
Aditya Agarwal <aditya.7@outlook.com>
2021-03-06 03:56:21 +0530

    Designed the landing page,header and footer, implemented CSS, changed local files to CDNs

---
+++ landing-nav.php
@@ -0,0 +1,16 @@
+<nav id="landing-nav" class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="index.php">
+    <span id="brand-title">U<span style="color:#ee786c; font-size: 40px">P</span>rocess</span> 
+  </a>
+
+  <div class=" navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a id="sign-in-link" class="nav-link" href="sign-in.php">Sign In</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link theme-button theme-button-clear" href="sign-up-company.php">Register</a>
+      </li>
+    </ul>
+  </div>
+</nav>
