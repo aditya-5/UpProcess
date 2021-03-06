a85ef08cb99a03150f35f586e08c629d612554d0
Aditya Agarwal <aditya.7@outlook.com>
2021-03-06 11:32:03 +0530

    Error message

--- employee/notes.php
+++ employee/notes.php
@@ -5,10 +5,21 @@
     </div>
     <div class="emp-slide-right">
         <div class="main-content-emp">
+        	
+        	<!-- Alert for no tasks found -->
+        	<div class="container">
+	        	<div class="alert alert-danger" style="border-radius: 30px; height:130px; text-align: center; font-size: 2em; padding-top: 35px; background-color: #EE786C; opacity: 50%;color: white; border: 1px solid #1F1F26" role="alert">
+					  Oops! Couldn't retrieve any tasks <br><br>
+					  Please add new tasks
+				</div>
+			</div>
+			<!-- END -->
 
         </div>
     </div>
 </div>
+
+
 
 
 
